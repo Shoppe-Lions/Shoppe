a7d1954867cebb8f0cea58b844531d2194ae6e0b
Igor <klevzhits94@icloud.com>
2025-03-02 21:44:01 +0300

    Initial Commit

---
+++ Shoppe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shoppe
+//
+//  Created by Игорь Клевжиц on 02.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
