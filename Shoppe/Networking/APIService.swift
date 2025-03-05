e69f7433da15b3aa824e3c7b5de73a7b4b0c0a92
Igor <klevzhits94@icloud.com>
2025-03-05 04:12:19 +0300

    SHOP-21: Add subcategories and likes

--- Shoppe/Networking/APIService.swift
+++ Shoppe/Networking/APIService.swift
@@ -11,26 +11,45 @@ final class APIService {
     private let baseURL = "https://fakestoreapi.com/products"
 
     func fetchProducts(completion: @escaping (Result<[Product], NetworkError>) -> Void) {
-        NetworkManager.shared.fetchData(from: baseURL, completion: completion)
-    }
+            NetworkManager.shared.fetchData(from: baseURL) { (result: Result<[Product], NetworkError>) in
+                switch result {
+                case .success(var products):
+                    
+                    for (index, _) in products.enumerated() {
+                        switch index {
+                        case 0:
+                            products[index].subcategory = "Backpacks"
+                        case 1...3:
+                            products[index].subcategory = "Clothing"
+                        case 4, 7:
+                            products[index].subcategory = "Cheap"
+                        case 5...6:
+                            products[index].subcategory = "Expensive"
+                        case 8...11:
+                            products[index].subcategory = "Hard Drives"
+                        case 12...13:
+                            products[index].subcategory = "Monitors"
+                        case 14...16:
+                            products[index].subcategory = "Warm Clothing"
+                        case 17...19:
+                            products[index].subcategory = "Light Clothing"
+                        default:
+                            products[index].subcategory = nil
+                        }
+                    }
+                    
+                    for index in products.indices {
+                        products[index].like = false
+                    }
+                    completion(.success(products))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
 
     func fetchProduct(by id: Int, completion: @escaping (Result<Product, NetworkError>) -> Void) {
         let url = "\(baseURL)/\(id)"
         NetworkManager.shared.fetchData(from: url, completion: completion)
     }
 }
-
-//final class ProductListInteractor {
-//    private let apiService = APIService()
-//
-//    func loadProducts(completion: @escaping ([Product]?) -> Void) {
-//        apiService.fetchProducts { result in
-//            switch result {
-//            case .success(let products):
-//                completion(products)
-//            case .failure:
-//                completion(nil)
-//            }
-//        }
-//    }
-//}
