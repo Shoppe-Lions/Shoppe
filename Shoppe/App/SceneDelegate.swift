7c47edb4e441c44742ab7b0a0b2e48b3b1fd0bbb
Igor <klevzhits94@icloud.com>
2025-03-07 02:54:24 +0300

    SHOP-6: Move product in Mouduls folder

--- Shoppe/App/SceneDelegate.swift
+++ Shoppe/App/SceneDelegate.swift
@@ -68,7 +68,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         tabBarController.tabBar.unselectedItemTintColor = .blue
         
-        window?.rootViewController = OnboardingViewController() // сюда вставить свой контроллер OnboardingViewController()
+        window?.rootViewController = tabBarController // сюда вставить свой контроллер OnboardingViewController()
         window?.makeKeyAndVisible()
     }
 
