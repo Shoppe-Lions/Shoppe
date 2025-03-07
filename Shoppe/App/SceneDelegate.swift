9314fbb12be2ba9635ba71c55a2234f68fdcb796
dkotikova <dskotikova@gmail.com>
2025-03-07 12:52:55 +0300

    SHOP-1: Add onboarding completion check using UserDefaults

--- Shoppe/App/SceneDelegate.swift
+++ Shoppe/App/SceneDelegate.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
         window?.overrideUserInterfaceStyle = .light
         
+        let onboardingCompleted = UserDefaults.standard.bool(forKey: "onboardingCompleted")
+        
+        let onboardingViewController = OnboardingViewController()
+        
         let tabBarController = UITabBarController()
         
-     
         let homeViewController = ViewController()
         let wishlistViewController = WishlistRouter.createModule()
         let unknownViewController = ViewController() // Что это за экран??)
@@ -68,10 +70,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         tabBarController.tabBar.unselectedItemTintColor = .blue
         
-        window?.rootViewController = OnboardingViewController() // сюда вставить свой контроллер OnboardingViewController()
+        onboardingViewController.didFinishOnboarding = {
+            UserDefaults.standard.set(true, forKey: "onboardingCompleted")
+            
+            UIWindow.transition(with: self.window!, duration: 0.5) {
+                self.window?.rootViewController = tabBarController
+            }
+        }
+        
+        if onboardingCompleted {
+            window?.rootViewController = tabBarController
+        } else {
+            window?.rootViewController = onboardingViewController
+        }
+        
         window?.makeKeyAndVisible()
     }
-
-
+    
+    
 }
 
