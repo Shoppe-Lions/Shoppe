c1c281d81669aebc268b59fde8c45983e6701f96
dkotikova <dskotikova@gmail.com>
2025-03-12 19:18:49 +0300

    SHOP-23: add editButton, hide keyboard

--- Shoppe/Modules/Profile/ProfileViewController.swift
+++ Shoppe/Modules/Profile/ProfileViewController.swift
@@ -8,29 +8,29 @@
 import UIKit
 import SnapKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITextFieldDelegate {
     
-    private lazy var nameTextField: UITextField = {
-        return createTextField(placeholder: "Ваше Имя", keyboardType: .default, isSecure: false, autocapitalization: .words)
+    lazy var nameTextField: UITextField = {
+        return createTextField(placeholder: "Name", keyboardType: .default, isSecure: false)
     }()
     
-    private lazy var emailTextField: UITextField = {
-        return createTextField(placeholder: "gmail@example.com", keyboardType: .emailAddress, isSecure: false, autocapitalization: .none)
+    lazy var emailTextField: UITextField = {
+        return createTextField(placeholder: "Email", keyboardType: .emailAddress , isSecure: false)
     }()
     
-    private lazy var passwordTextField: UITextField = {
-        return createTextField(placeholder: "Введите пароль", keyboardType: .default, isSecure: true, autocapitalization: .none)
+    lazy var passwordTextField: CustomPasswordTextField = {
+        let textField = CustomPasswordTextField()
+        textField.backgroundColor = UIColor(named: "CustomLightGray")
+        textField.delegate = self
+        return textField
     }()
     
-    private lazy var profileStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [nameTextField, emailTextField, passwordTextField])
-        stack.axis = .vertical
-        stack.spacing = 16
-        stack.distribution = .fillEqually
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
+    lazy var editButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "EditButton"), for: .normal)
+        button.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+        return button
     }()
-    
     
     private let settingsLabel: UILabel = {
         let label = UILabel()
@@ -52,11 +52,9 @@ class ProfileViewController: UIViewController {
     
     private let profileImage: UIImageView = {
         let image = UIImageView()
-        image.backgroundColor = .red
         image.image = UIImage(named: "Profile")
         image.contentMode = .scaleAspectFill
         image.clipsToBounds = true
-        //        image.layer.cornerRadius = 100 / 2
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -64,7 +62,6 @@ class ProfileViewController: UIViewController {
     private let profileImageFrame: UIView = {
         let frame = UIView()
         frame.backgroundColor = .white
-        //        frame.layer.cornerRadius = 110 / 2
         frame.clipsToBounds = false
         frame.layer.shadowColor = UIColor.black.cgColor
         frame.layer.shadowOpacity = 0.3
@@ -74,19 +71,17 @@ class ProfileViewController: UIViewController {
         return frame
     }()
     
-    private let stackView: UIStackView = {
-        let stack = UIStackView()
-        return stack
-    }()
     
     private let saveButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .system)
+        button.setTitle("Save Changes", for: .normal)
         button.titleLabel?.font = UIFont(name: Fonts.NunitoSans.light, size: 18)
-        button.backgroundColor = .systemBlue
-        button.titleLabel?.textColor = .white
-        button.titleLabel?.text = "Save changes"
+        button.backgroundColor = .customBlue
+        button.setTitleColor(.white, for: .normal)
         button.layer.masksToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(nil, action: #selector(saveButtonTapped), for: .touchUpInside)
+        
         return button
     }()
     
@@ -94,6 +89,9 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         setupView()
         setupConstraints()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
     override func viewDidLayoutSubviews() {
@@ -102,25 +100,20 @@ class ProfileViewController: UIViewController {
         DispatchQueue.main.async {
             self.profileImageFrame.layer.cornerRadius = self.profileImageFrame.frame.height / 2
             self.profileImage.layer.cornerRadius = self.profileImage.frame.height / 2
+            self.saveButton.layer.cornerRadius = self.saveButton.frame.height * 0.25
         }
     }
     
+    @objc private func saveButtonTapped() {
+        print("Изменения сохранены")
+    }
     
-    private func createTextField(placeholder: String, keyboardType: UIKeyboardType, isSecure: Bool, autocapitalization: UITextAutocapitalizationType) -> UITextField {
-        let textField = UITextField()
-        textField.placeholder = placeholder
-        textField.keyboardType = keyboardType
-        textField.isSecureTextEntry = isSecure
-        textField.autocapitalizationType = autocapitalization
-        textField.layer.cornerRadius = 15
-        textField.clipsToBounds = true
-        textField.backgroundColor = UIColor(named: "CustomLightGray")
-        textField.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
-        textField.leftView = leftPaddingView
-        textField.leftViewMode = .always
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
+    @objc private func editButtonTapped() {
+        print("Кнопка редактирования нажата")
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
 }
@@ -135,9 +128,13 @@ extension ProfileViewController {
         view.addSubview(profileLabel)
         view.addSubview(profileImageFrame)
         profileImageFrame.addSubview(profileImage)
-                view.addSubview(profileStackView)
+        profileImageFrame.addSubview(editButton)
+        view.addSubview(nameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        
         view.addSubview(saveButton)
-
+        
     }
 }
 
@@ -145,30 +142,24 @@ extension ProfileViewController {
 extension ProfileViewController {
     
     private func setupConstraints() {
-        //
-        //        // надо создать какое-нибудь небольшое число которое будет зависеть от размера экрана и его потом умножать
+        
         let horizontalPadding = UIScreen.main.bounds.width * 0.05
         let verticalPadding = UIScreen.main.bounds.height * 0.05
-        //
-        //
+        let textFieldVerticalPadding = UIScreen.main.bounds.height * 0.02
+        
         settingsLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(verticalPadding * 1)
             make.leading.equalToSuperview().inset(horizontalPadding)
         }
-        //          ((( ( make.leading.trailing.equalTo(pageView).inset(horizontalPadding) )))))
         
         profileLabel.snp.makeConstraints { make in
-            make.top.equalTo(settingsLabel.snp.bottom).offset(verticalPadding / 2)
-            make.leading.equalToSuperview().inset(horizontalPadding)            //
+            make.top.equalTo(settingsLabel.snp.bottom).offset(verticalPadding / 4)
+            make.leading.equalToSuperview().inset(horizontalPadding)
         }
         
-        // (((((( make.bottom.lessThanOrEqualTo(pageButton.snp.top).offset(-verticalPadding) )))))))
-        //        }
-        //
         profileImageFrame.snp.makeConstraints { make in
             make.top.equalTo(profileLabel.snp.bottom).offset(verticalPadding / 2)
-            make.height.equalTo(view).multipliedBy(0.14)
-            make.width.equalTo(profileImageFrame.snp.height)
+            make.width.height.equalTo(PLayout.horizontalPadding * 5)
             make.leading.equalToSuperview().inset(horizontalPadding)
         }
         
@@ -177,51 +168,77 @@ extension ProfileViewController {
             make.centerX.centerY.equalTo(profileImageFrame)
         }
         
-        profileStackView.snp.makeConstraints { make in
-            make.top.equalTo(profileImageFrame.snp.bottom).offset(verticalPadding / 2)
-            make.leading.trailing.equalToSuperview().inset(horizontalPadding)
-            make.height.lessThanOrEqualTo(UIScreen.main.bounds.height * 0.2)
-            make.bottom.lessThanOrEqualToSuperview().offset(-verticalPadding * 2)
+        editButton.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+            make.trailing.equalTo(profileImageFrame).offset(1)
         }
         
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageFrame.snp.bottom).offset(verticalPadding / 2)
+            make.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            make.height.equalTo(PLayout.horizontalPadding * 2.5)
+        }
         
-        //
-        //          (этого еще нет)
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(nameTextField.snp.bottom).offset(textFieldVerticalPadding / 2)
+            make.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            make.height.equalTo(PLayout.horizontalPadding * 2.5)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(textFieldVerticalPadding / 2)
+            make.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            make.height.equalTo(PLayout.horizontalPadding * 2.5)
+        }
+        
         saveButton.snp.makeConstraints { make in
-                    make.leading.trailing.equalTo(view).inset(horizontalPadding)
-                    make.height.equalTo(UIScreen.main.bounds.height * 0.06)
-                    make.bottom.lessThanOrEqualTo(view).offset(-verticalPadding * 0.5)
-                }
+            make.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            make.height.equalTo(PLayout.horizontalPadding * 2.5)
+            make.bottom.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.bottom).offset(-verticalPadding * 1)
+        }
     }
     
     
+}
+// MARK: - Create TextField
+extension ProfileViewController {
+    
+    private func createTextField(placeholder: String, keyboardType: UIKeyboardType, isSecure: Bool) -> UITextField {
+        let textField = UITextField()
+        textField.backgroundColor = UIColor(named: "CustomLightGray")
+        textField.font = UIFont(name: "NunitoSans10pt-Regular", size: PFontSize.normal)
+        textField.placeholder = placeholder
+        textField.isSecureTextEntry = isSecure
+        textField.keyboardType = keyboardType
+        textField.layer.cornerRadius = 12
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.delegate = self
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }
+    
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+        textField.layer.borderWidth = 2
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.layer.borderWidth = 1
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     
 }
-//
-//  private func setupConstraints() {
-//        NSLayoutConstraint.activate ([
-//            settingsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-//            settingsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-//
-//            profileLabel.topAnchor.constraint(equalTo: settingsLabel.bottomAnchor, constant: 7),
-//            profileLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-//
-//            profileImageFrame.heightAnchor.constraint(equalToConstant: 110),
-//            profileImageFrame.widthAnchor.constraint(equalTo: profileImageFrame.heightAnchor),
-//            profileImageFrame.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-//            profileImageFrame.topAnchor.constraint(equalTo: profileLabel.bottomAnchor, constant: 20),
-//
-//            profileImage.widthAnchor.constraint(equalToConstant: 100),
-//            profileImage.heightAnchor.constraint(equalTo: profileImage.widthAnchor),
-//            profileImage.centerXAnchor.constraint(equalTo: profileImageFrame.centerXAnchor),
-//            profileImage.centerYAnchor.constraint(equalTo: profileImageFrame.centerYAnchor),
-//
-//            profileStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            profileStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-//            profileStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-//            profileStackView.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20),
-//            //profileStackView.heightAnchor.constraint(equalToConstant: 180)
-//
-//        ])
-//    }
+
 
